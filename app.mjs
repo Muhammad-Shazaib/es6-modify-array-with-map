4f9a3bbe202e4b219c17d3bbf5e54ff070adf5c0
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-06-01 01:16:03 +0500

    update

--- app.mjs
+++ app.mjs
@@ -8,14 +8,14 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let newArr = arr.map((eachItem, i) => {
     console.log(eachItem)
-    
-if(eachItem % 2 === 0) { // even number
-    return eachItem 
-   
-}else { // odd number
-    return eachItem * 2
-}
-    
+
+    if (eachItem % 2 === 0) { // even number
+        return eachItem
+
+    } else { // odd number
+        return eachItem * 2
+    }
+
 })
 
 console.log(arr)
