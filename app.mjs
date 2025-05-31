8a1297de60fac0b9b12b75355b6722051c71368c
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-06-01 01:04:55 +0500

    updated

--- app.mjs
+++ app.mjs
@@ -4,4 +4,19 @@
 // for loop se sary kam ho jaty hai.
 
 
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+let newArr = arr.map((eachItem, i) => {
+    console.log(eachItem)
+    
+if(eachItem % 2 === 0) { // even number
+    return eachItem 
+   
+}else { // odd number
+    return eachItem * 2
+}
+    
+})
+
+console.log(arr)
+console.log(newArr)
